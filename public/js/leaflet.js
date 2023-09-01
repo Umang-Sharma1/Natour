7545ab6d6919c74603aa6d236de4dff7817da031
Umang-Sharma1 <hemangsharma1000@gmail.com>
2023-09-01 17:40:43 +0530

    Initial commit

---
+++ public/js/leaflet.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+export const displayMap = (locations) => {
+  let map = L.map('map', { zoomControl: false });
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  var greenIcon = L.icon({
+    iconUrl: '/img/pin.png',
+    iconSize: [32, 40],
+    iconAnchor: [16, 45],
+    popupAnchor: [0, -50],
+  });
+
+  const points = [];
+  locations.forEach((loc) => {
+    // Create points
+    points.push([loc.coordinates[1], loc.coordinates[0]]);
+
+    // Add markers
+    L.marker([loc.coordinates[1], loc.coordinates[0]], { icon: greenIcon })
+      .addTo(map)
+      // Add popup
+      .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+        autoClose: false,
+      })
+      .openPopup();
+  });
+
+  const bounds = L.latLngBounds(points).pad(0.5);
+  map.fitBounds(bounds);
+
+  map.scrollWheelZoom.disable();
+};
---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in successfully');
+      setTimeout(() => window.location.assign('/'), 1000);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+    });
+
+    if (res.data.status === 'success') {
+      window.location.assign('/login');
+      // window.location.reload(true);
+    }
+  } catch (err) {
+    showAlert('error', 'Error Logging Out! Try Again');
+  }
+};
